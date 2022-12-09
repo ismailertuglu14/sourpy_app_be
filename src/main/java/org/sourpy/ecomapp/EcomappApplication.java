a9ea4c57b72617933ac04e459913aec71f0f5472
Emre Şahna <emre.sahna@hotmail.com>
2022-12-09 23:05:52 +0300

    cors config

--- src/main/java/org/sourpy/ecomapp/EcomappApplication.java
+++ src/main/java/org/sourpy/ecomapp/EcomappApplication.java
@@ -2,6 +2,9 @@ package org.sourpy.ecomapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EcomappApplication {
@@ -10,4 +13,11 @@ public class EcomappApplication {
 		SpringApplication.run(EcomappApplication.class, args);
 	}
 
+	@Configuration
+	public class WebConfiguration implements WebMvcConfigurer {
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**").allowedMethods("*");
+		}
+	}
 }
